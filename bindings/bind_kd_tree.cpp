a89292887f17b3539007f7d35ea96a1025846075
William Bell <wjbell97@gmail.com>
2021-04-01 13:41:07 -0400

    Removed commented out blocks and some wrapping classes in bindings files that turned out to not be necessary

--- bindings/bind_kd_tree.cpp
+++ bindings/bind_kd_tree.cpp
@@ -5,51 +5,15 @@
 
 namespace py = pybind11;
 
-struct WrapkdTree {
-public:
-    WrapkdTree(Eigen::Ref<const Eigen::MatrixXd> states)
-    {
-        tree = kdTree{states};
-    }
-
-    void append_state(Eigen::Ref<const Eigen::VectorXd> state)
-    {
-        tree.append_state(state);
-    }
-
-    Eigen::VectorXd nearest_neighbor(Eigen::Ref<const Eigen::VectorXd> state) const
-    {
-        return tree.nearest_neighbor(state);
-    }
-
-    Eigen::MatrixXd k_nearest_neighbors(Eigen::Ref<const Eigen::MatrixXd> state, int k) const
-    {
-        return tree.k_nearest_neighbors(state, k);
-    }
-
-    int count_states() const 
-    {
-        return tree.count_states();
-    }
-
-    Eigen::MatrixXd at_depth(int depth) const
-    {
-        return tree.at_depth(depth);
-    }
-
-private:
-    kdTree tree;
-};
-
 void init_kd_tree(py::module_ &m)
 {
-    py::class_<WrapkdTree>(m, "KD_Tree")
+    py::class_<kdTree>(m, "KD_Tree")
         .def(py::init<Eigen::Ref<const Eigen::MatrixXd>>())
-        .def("append_state", &WrapkdTree::append_state)
-        .def("nearest_neighbor", &WrapkdTree::nearest_neighbor)
-        .def("k_nearest_neighbors", &WrapkdTree::k_nearest_neighbors)
-        .def("count_states", &WrapkdTree::count_states)
-        .def("at_depth", &WrapkdTree::at_depth);
+        .def("append_state", &kdTree::append_state)
+        .def("nearest_neighbor", &kdTree::nearest_neighbor)
+        .def("k_nearest_neighbors", &kdTree::k_nearest_neighbors)
+        .def("count_states", &kdTree::count_states)
+        .def("at_depth", &kdTree::at_depth);
     
     py::register_exception<BadStateSizeException>(m, "BadStateSizeException");
     py::register_exception<EmptyTreeException>(m, "EmptyTreeException");
--- bindings/bind_roadmap.cpp
+++ bindings/bind_roadmap.cpp
@@ -7,46 +7,6 @@
 
 namespace py = pybind11;
 
-// struct WrapRoadmap {
-//     WrapRoadmap() {}
-    
-//     WrapRoadmap(const Roadmap& roadmap_to_wrap) {
-//         roadmap = roadmap_to_wrap;
-//     }
-
-//     Roadmap roadmap;
-
-//     void add_node(Eigen::Ref<const Eigen::VectorXd> state, Eigen::Ref<const Eigen::MatrixXd> neighbor_states, 
-//         Eigen::Ref<const Eigen::VectorXd> neighbor_costs) {
-        
-//         roadmap.add_node(state, neighbor_states, neighbor_costs);
-//     }
-
-//     std::vector<RoadmapNode> nodes() {
-//         return roadmap.nodes();
-//     }
-
-//     RoadmapNode node_at(Eigen::Ref<const Eigen::VectorXd> state) {
-//         return roadmap.node_at(state);
-//     }
-
-//     RoadmapNode node_nearest(Eigen::Ref<const Eigen::VectorXd> state) {
-//         return roadmap.node_nearest(state);
-//     }
-
-//     std::vector<RoadmapNode> k_nodes_nearest(Eigen::Ref<const Eigen::VectorXd> state, int k) {
-//         return roadmap.k_nodes_nearest(state, k);
-//     }
-
-//     Eigen::VectorXd state_nearest(Eigen::Ref<const Eigen::VectorXd> state) {
-//         return roadmap.state_nearest(state);
-//     }
-
-//     Eigen::MatrixXd k_states_nearest(Eigen::Ref<const Eigen::VectorXd> state, int k) {
-//         return roadmap.k_states_nearest(state, k);
-//     }
-// };
-
 void init_roadmap(py::module_& m)
 {
     py::class_<RoadmapNode>(m, "RoadMapNode")
@@ -54,16 +14,6 @@ void init_roadmap(py::module_& m)
         .def_property_readonly("state", &RoadmapNode::getState)
         .def_property_readonly("neighbors", &RoadmapNode::getNeighbors)
         .def_property_readonly("costs", &RoadmapNode::getCosts);
-
-    // py::class_<WrapRoadmap>(m, "RoadMap")
-    //     .def(py::init<>())
-    //     .def("add_node", &WrapRoadmap::add_node)
-    //     .def("nodes", &WrapRoadmap::nodes)
-    //     .def("node_at", &WrapRoadmap::node_at)
-    //     .def("node_nearest", &WrapRoadmap::node_nearest)
-    //     .def("k_nodes_nearest", &WrapRoadmap::k_nodes_nearest)
-    //     .def("state_nearest", &WrapRoadmap::state_nearest)
-    //     .def("k_states_nearest", &WrapRoadmap::k_states_nearest);
 
     py::class_<Roadmap>(m, "RoadMap")
         .def(py::init<>())
